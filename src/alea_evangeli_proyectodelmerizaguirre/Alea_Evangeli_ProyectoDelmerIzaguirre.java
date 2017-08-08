95ebcd554149fd62d9166e6480689634e800b652
Delmer Izaguirre <delmerespinal2@gmail.com>
2017-08-07 21:05:27 -0600

    "comienzo"

---
+++ src/alea_evangeli_proyectodelmerizaguirre/Alea_Evangeli_ProyectoDelmerIzaguirre.java
@@ -0,0 +1,18 @@
+
+package alea_evangeli_proyectodelmerizaguirre;
+
+/**
+ *
+ * @author Delmer
+ */
+public class Alea_Evangeli_ProyectoDelmerIzaguirre {
+
+    public static void main(String[] args) {
+        Tablero tab = new Tablero();
+        
+     //   tab.moverPieza(3, 3, 19, 19, '#');
+     //   tab.ImprimirTablero(0, 0);
+        
+    }
+    
+}
